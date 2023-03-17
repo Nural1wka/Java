49df258a34ad5ba2f26b7477a2192be712f5a9cc
Nural1wka <128140193+Nural1wka@users.noreply.github.com>
2023-03-17 17:36:22 +0600

    Add files via upload

---
+++ homework/6th week/People/People.java
@@ -0,0 +1,13 @@
+package People;
+
+public class People {
+    public static void main(String[] args){
+        Person negr = new Person("Alina", "Bew");
+        System.out.println(negr);
+        Student graphDrakula = new Student("Ben", "Moscow", "Computer Science", 3, 2.28);
+        System.out.println(graphDrakula);
+        Staff lord = new Staff("Galina", "Argentina", "N3", 200000.222);
+        System.out.println(lord);
+    }
+}
+
---
+++ homework/6th week/People/Person.java
@@ -0,0 +1,28 @@
+package People;
+
+class Person {
+    private String name;
+    private String address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "People.Person[ name = " + name + ", address = " + address + " ]";
+    }
+}
