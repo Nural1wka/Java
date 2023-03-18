66cad3c390f3c0c63b0ab34d4d2a74cda857b247
Nural1wka <128140193+Nural1wka@users.noreply.github.com>
2023-03-18 11:34:15 +0600

    Update MovableCircle.java

--- homework/7th week/MovingMovingWithMovableCircle/MovableCircle.java
+++ homework/7th week/MovingMovingWithMovableCircle/MovableCircle.java
@@ -1,35 +1,35 @@
 package MovingMovingWithMovableCircle;
 
-public class MovableCircle extends MovablePoint implements Movable{
+public class MovableCircle implements Movable{
     private int radius;
     private MovablePoint center;
     public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius){
-        super(x, y, xSpeed, ySpeed);
+        center = new MovablePoint(x, y, xSpeed, ySpeed);
         this.radius = radius;
     }
 
     @Override
     public String toString() {
-        return super.toString() + ", radius = " + radius;
+        return center.toString() + ", radius = " + radius;
     }
 
     @Override
     public void moveUp() {
-        super.moveUp();
+        center.moveUp();
     }
 
     @Override
     public void moveDown() {
-        super.moveDown();
+        center.moveDown();
     }
 
     @Override
     public void moveLeft() {
-        super.moveLeft();
+        center.moveLeft();
     }
 
     @Override
     public void moveRight() {
-        super.moveRight();
+        center.moveRight();
     }
 }
